2f60454cec329021f9d2f3a1e68598b08e34f74d
giubertsanchez <56102914+giubertsanchez@users.noreply.github.com>
2025-08-04 12:35:15 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://giubertphone.github.io',
-  base: 'nat',
+  //removing base to see if it is casuing link issues
+  // base: 'nat',
 
   // For GitHub Pages deployment
   // This is the repository name where the site will be hosted
