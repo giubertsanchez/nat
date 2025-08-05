c1cac2fbd80322aaa4a20e2240632b2d3c2fc895
giubertsanchez <56102914+giubertsanchez@users.noreply.github.com>
2025-08-05 09:10:17 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
   export default defineConfig({
-  site: 'https://giubertsanchez.github.io',
-  base:"https://giubertsanchez.github.io/nat/",
   
   vite: {
     plugins: [tailwindcss()],
